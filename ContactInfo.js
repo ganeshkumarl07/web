e7d70918a2b925de011fca4a8ce1f932224ad17f
GANESHKUMAR L <109757386+ganeshkumarl07@users.noreply.github.com>
2025-03-08 04:31:09 +0530

    Create ContactInfo.js

---
+++ ContactInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactInfo = () => (
+  <div className="p-4">
+    <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+    <p>Email: info@webdev.com</p>
+    <p>Phone: +123 456 7890</p>
+  </div>
+);
+
+export default ContactInfo;
